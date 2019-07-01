2c51952ddf310a8ab6352bdd6fce06c3ad4b3d4e
Liying Wang <jaybin502@gmail.com>
2019-06-30 22:15:33 -0700

    update for map

--- analysis/paper/map.R
+++ analysis/paper/map.R
@@ -19,30 +19,30 @@ Kiwulan <- data.frame(lon = c(121.7809), lat = c(24.8066))
 
 TW_SE_Asia <-
 ggplot(data = world) +
-  geom_sf() +
+  geom_sf(fill= "antiquewhite") +
   geom_rect(xmin = 119.8, xmax = 122.2, ymin = 21.7, ymax = 25.5,
             fill = NA, colour = "red", size = 0.5) +
-  coord_sf(xlim = c(107, 135), ylim = c(13, 35), expand = FALSE, datum = NA)
-
-TW_SE_Asia_void <-
-  TW_SE_Asia +
-  theme_void() +
-  theme(panel.border = element_rect(colour = "black",
-                                    fill = NA))
+  coord_sf(xlim = c(107, 135), ylim = c(13, 35), expand = FALSE, datum = NA) +
+  theme(panel.background = element_rect(fill = "azure"))
 
 library("ggspatial")
 TW_map_with_site <-
 ggplot(data = world) +
-  geom_sf(data = TW_map) +
+  geom_sf(data = TW_map, fill= "antiquewhite") +
   geom_point(data = Kiwulan, aes(x = lon, y = lat),
              size = 3, color = "red") +
-  geom_text(aes(x = lon, y = lat, label = "Kiwulan"),
-            data = Kiwulan, position = position_nudge(y = -0.1)) +
+  geom_text(data = Kiwulan,
+            aes(x = lon, y = lat, label = "Kiwulan"),
+            fontface = "bold",
+            position = position_nudge(y = -0.15)) +
   coord_sf(xlim = c(118, 122.2), ylim = c(21.9, 25.4), expand = FALSE) +
   annotation_scale(location = "bl", width_hint = 0.5) +
   annotation_north_arrow(location = "bl", which_north = "true",
                          pad_x = unit(0.25, "in"), pad_y = unit(0.45, "in"),
-                         style = north_arrow_fancy_orienteering)
+                         style = north_arrow_fancy_orienteering) +
+  theme(panel.background = element_rect(fill = "azure"),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank())
 
 TW_map_with_site +
   annotation_custom(
@@ -50,4 +50,4 @@ TW_map_with_site +
     xmin = 118,
     xmax = 120,
     ymin = 23.5,
-    ymax = 25.5)
+    ymax = 25.7)
