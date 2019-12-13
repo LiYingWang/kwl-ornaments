7cefae231a6aeb11260475c9d015e4864bdb20da
Liying Wang <jaybin502@gmail.com>
2019-12-13 11:11:42 -0800

    test the correlation between ornaments and potsherds, add one plot

--- analysis/paper/potsherds.R
+++ analysis/paper/potsherds.R
@@ -75,7 +75,7 @@ kwl_lp_clean <-
 kwl_upper <-
   readxl::read_excel(here("analysis", "data", "raw_data", "Kiwulan_Ornament_Upper.xlsx"))
 
-## clean the ornament data and assign periods
+## clean the ornament data, assign periods
 ornaments_period <-
   kwl_upper %>%
   filter(!is.na(`6-layers`)) %>%
@@ -93,50 +93,38 @@ ornaments_period <-
   mutate(join_id = paste0(Pit_No,"-",Layer)) %>%
   filter(Categories != "Unknown Metal") %>%
   mutate(Categories = fct_lump(Categories,
-                               n = 5)) %>%
+                               n = 5))
+
+## join with potsherds data
+ornaments_potsherds <-
+  ornaments_period %>%
   left_join(kwl_lp_clean, "join_id") %>%
   rename(potsherds_weight = "重量小計") %>%
+  mutate(potsherds_weight = as.numeric(potsherds_weight)) %>%
   group_by(join_id, period, potsherds_weight) %>%
-  count(Layer)
+  count(Layer) %>%
+  select(-Layer) %>%
+  rename(ornament_piece = "n") %>%
+  group_by(period) %>%
+  summarise(sum_pot= sum(potsherds_weight, na.rm = TRUE),
+            sum_orna= sum(ornament_piece, na.rm = TRUE))
 
-#
-kwl_lp_clean_summary <-
-  kwl_lp_clean %>%
-  group_by(end) %>%
-  summarise(mean_weight= mean(重量小計, na.rm = TRUE))
+## plot
+plot_ornaments_potsherds <-
+  ornaments_potsherds %>%
+  pivot_longer(-period,
+               names_to = "variable",
+               values_to = "value") %>%
+  ggplot() +
+  geom_col(aes(variable, value)) +
+  facet_wrap(~ period) +
+  scale_y_log10() +
+  labs(x = "Artifact",
+       y = "Amount") +
+  theme_minimal(base_size = 10)
 
-kwl_lp_clean_piece <-
-  kwl_lp_clean %>%
-  group_by(`編號\n/ 現象號(P)`) %>%
-  summarise(sum_p= sum(件數小計, na.rm = TRUE))
-
-# plot
-ggplot(kwl_lp_clean,
-       aes(x = end,
-           y = 重量小計)) +
-  geom_line() +
-  facet_wrap(~`編號\n/ 現象號(P)`)
-
-ggplot(kwl_lp_clean,
-       aes(x = end,
-           y = 重量小計,
-           colour = `編號\n/ 現象號(P)`)) +
-  geom_line()
-
-ggplot() +
-  geom_line(data = kwl_lp_clean,
-            aes(x = end,
-                y = 重量小計,
-                colour = `編號\n/ 現象號(P)`),
-            alpha = 0.4) +
-  geom_line(data = kwl_lp_clean_summary,
-            aes(end,
-                mean_weight),
-            size = 1.5) +
-  labs(colour = "Units",
-       x = "Depth (cm)",
-       y = "Weights of Sherds") +
-  theme_minimal()
-
-ggsave("pottery-per-level.png", w = 7, h = 5.5, dpi = 600)
-
+## test
+chisq.test(ornaments_potsherds)
+library(Hmisc)
+rcorr(ornaments_potsherds, type="pearson")
+t.test(ornaments_potsherds$sum_orna, ornaments_potsherds$sum_pot)
